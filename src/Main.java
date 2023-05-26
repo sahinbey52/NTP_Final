468060e5990047749e58ebc3f9dabf926f8b7545
Ayşenur Yörür <92437276+AysenurYrr@users.noreply.github.com>
2023-05-26 20:35:11 +0300

    Örnek arayüz, kamera bölümü eksik.

--- src/Main.java
+++ src/Main.java
@@ -1,146 +1,116 @@
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import javax.imageio.ImageIO;
+import org.bytedeco.javacv.*;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.MatVector;
+import org.bytedeco.opencv.opencv_core.Rect;
+import org.bytedeco.opencv.opencv_core.Scalar;
+import org.bytedeco.opencv.opencv_core.Size;
+import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
+import org.bytedeco.opencv.opencv_videoio.VideoCapture;
+
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import com.restfb.DefaultFacebookClient;
-import com.restfb.FacebookClient;
-import org.bytedeco.javacv.FrameGrabber;
-import org.bytedeco.javacv.Java2DFrameConverter;
-import org.bytedeco.javacv.OpenCVFrameGrabber;
-import java.util.Scanner;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 
-public class Main extends JFrame{
 
-    public boolean cek;
+public class Main {
+
+    static OpenCVFrameGrabber grabber;
+    BufferedImage foto;
+    File resim_dosyasi;
+
     public static void main(String[] args) {
-        Main mn =new Main();
-        mn.cek=true;
+        // Kamera bağlantısını oluştur
+        grabber = new OpenCVFrameGrabber(0);
 
-        JFrame frm=new JFrame();
-        JPanel panel = new JPanel(new GridLayout(2,2));
-        JLabel img=new JLabel();
-        panel.add(img);
+        Dimension fotoBoyut = new Dimension(640, 480);
+        grabber.setImageWidth(fotoBoyut.width);
+        grabber.setImageHeight(fotoBoyut.height);
 
-        JButton btn=new JButton("Çek");
-        panel.add(btn);
-        JButton btnkaydet =new JButton("Kaydet");
-        panel.add(btnkaydet);
-        JButton btntw=new JButton("Twitter");
-        panel.add(btntw);
-        JButton btnsb=new JButton("SB yap");
-        panel.add(btnsb);
-        JButton btnparlat=new JButton("parlat");
-        panel.add(btnparlat);
-        JButton btnkontr=new JButton("Kontrast");
-        panel.add(btnkontr);
-        JButton btnblnk=new JButton("Bulanık");
-        panel.add(btnblnk);
+        try {grabber.start();}
+        catch (Exception e) {e.printStackTrace();}
 
-        /*JCheckBox cb_sb=new JCheckBox("Siyah-beyaz");
-        JCheckBox cb_parlat=new JCheckBox("Parlat");
-        JCheckBox cb_kontr=new JCheckBox("Kontrast");
-        JCheckBox cb_blnk=new JCheckBox("Bulanıklaştır");
-        panel.add(cb_sb);
-        panel.add(cb_parlat);
-        panel.add(cb_kontr);
-        panel.add(cb_blnk);*/
+        // CanvasFrame oluşturma
+        CanvasFrame fram = new CanvasFrame("Kamera Görüntüsü");
+
+        // JFrame ve JLabel oluşturma
+        JFrame frame = new JFrame("Fotoğraf Uygulaması");
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); // Tam ekran modunda başlatma
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setLayout(new BorderLayout());
 
 
-        frm.getContentPane().add(panel);
-        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frm.setSize(640, 480);
+        //Sol üstteki fotoğraf bölümü
+        if (fram.isVisible()) {
+            // Kameradan bir frame al
+            try {
+                Frame capturedFrame = grabber.grab();
+                // Frame'i ekrana göster
+                fram.showImage(capturedFrame);
+                JLabel imageLabel = new JLabel(new ImageIcon("resim.jpg"));
+                frame.add(imageLabel, BorderLayout.WEST);
+            }
+            catch (Exception e) {e.printStackTrace();
+            }
+            // Sağdaki butonlar bölümü
+            JPanel buttonsPanel = new JPanel(new GridLayout(3, 1));
+            JButton aydinlatButton = new JButton("Aydınlat");
+            buttonsPanel.add(aydinlatButton);
 
-        frm.setVisible(true);
+            JButton parlatButton = new JButton("Parlat");
+            buttonsPanel.add(parlatButton);
 
-        Filtre ft=new Filtre();
+            JButton grilestirButton = new JButton("Grileştir");
+            buttonsPanel.add(grilestirButton);
 
-        btn.addActionListener(new ActionListener() {
+            frame.add(buttonsPanel, BorderLayout.CENTER);
+
+        aydinlatButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                mn.cek=false;
-                ft.fotografCek();
+                // Aydınlat butonuna tıklandığında yapılacak işlemler
             }
         });
 
-        btnkaydet.addActionListener(new ActionListener() {
+        parlatButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("butona tıklandı");
-                ft.fotografKaydet();
-
-            }
-        });
-        btntw.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ft.fotoPaylas();
-            }
-        });
-        btnsb.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ft.siyah_beyaz(img);
-
-            }
-        });
-        btnparlat.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ft.parlat(img);
-
-            }
-        });
-        btnkontr.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ft.kontrast(img);
-
-            }
-        });
-        btnblnk.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ft.bulanik(img);
-
+                // Parlat butonuna tıklandığında yapılacak işlemler
             }
         });
 
-        /*cb_sb.addActionListener(new ActionListener() {
+        grilestirButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Siyah beyaz yapıldı");
+                // Grileştir butonuna tıklandığında yapılacak işlemler
             }
         });
-        cb_kontr.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Kontrast ayarlandı");
-            }
-        });
-        cb_parlat.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Parlatıldı");
-            }
-        });
-        cb_blnk.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Bulanıklaştırıldı");
-            }
-        });*/
 
-        //FacebookClient fbc=new DefaultFacebookClient();
+        // Altta bulunan butonlar bölümü
+        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
-        while (mn.cek){
-            ft.canli(img);
-        }
+        JButton kaydetButton = new JButton("Kaydet");
+        bottomPanel.add(kaydetButton);
 
+        JButton durdurButton = new JButton("Durdur");
+        bottomPanel.add(durdurButton);
 
+        JButton fotoCekButton = new JButton("Fotoğraf Çek");
+        bottomPanel.add(fotoCekButton);
+
+        frame.add(bottomPanel, BorderLayout.SOUTH);
+
+        // Sağ alttaki gönder butonu
+        JButton gonderButton = new JButton("Gönder");
+        frame.add(gonderButton, BorderLayout.EAST);
+
+        frame.setVisible(true);
     }
+}
 }
